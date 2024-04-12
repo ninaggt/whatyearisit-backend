ee2f615c3f9c26d00fec9bad1e0798051ffd3531
Your name <Your email>
2024-04-12 10:30:40 +0200

    my first commit

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/date', (req,res) =>{
+  const date = new Date ();
+  res.json({ date : date});
+});
+
+module.exports = router;
