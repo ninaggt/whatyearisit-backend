6f81eb73d797540b8aebcbbe849f19c5bccfeec4
Your name <Your email>
2024-04-12 11:53:03 +0200

    update date backend

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
-router.get('/date', (req,res) =>{
+router.get('/year', (req,res) =>{
   const date = new Date ();
   res.json({ date : date});
 });
